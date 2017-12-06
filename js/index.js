ac18578f046d72317071ff1858b74d242cc56b7b
Julie Sava <juli.evgenievna@gmail.com>
2017-12-06 20:30:42 +0000

    Add index.js

---
+++ js/index.js
@@ -0,0 +1,56 @@
+// //alert('Hello World!');
+// //document.write('Hello World!');
+// //console.log('Hello World!');
+
+// var messageForUser;
+// messageForUser = 'Hello';
+
+// document.write(messageForUser);
+
+// document.write('<br>');
+
+// var userAge = 20.5;
+// document.write(userAge);
+
+// let userProfession = 'Developer';
+// document.write(userProfession);
+
+// const favouriteColor = 'red';
+
+// //Data types
+
+// let msg = "Hello World!" // String
+// let age = 50; //Number
+// let isMale = true; //Boolen: true, false
+// let unknow = undefined; // underfined
+// let empty = null; // null
+// let obj = {}; // Oblect
+
+// //ES2015
+
+// let sym = Symbol(); // Symbol
+
+// const userName = prompt('Enter your name');
+// const greetings = 'Hello ' + userName;
+// document.write(greetings);
+
+// document.write('<br>');
+
+// const userAge = Number(prompt('Enter your age'));
+// const nextAge = userAge + 1;
+// document.write('Next year you will be ' + nextAge);
+// document.write('<br>');
+
+// if (userAge < 18) {
+//     document.write('Child');
+// } else {
+//     document.write('Adult');
+// }
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+const calc = sum(100, 200, 300);
+
+document.write(calc);
